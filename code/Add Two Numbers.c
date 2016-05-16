6f1677747aa02e1264501011078e5fd5d1d881a3
ethan <huangyitian@gwu.edu>
2016-05-16 07:55:49 -0400

    change

---
+++ code/Add Two Numbers.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+ 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+	int val;
+	struct ListNode *next;
+}ListNode;
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    ListNode* res;// = malloc(sizeof(l1));
+    return res;
+}
+
+int main(){
+	
+	return 0;
+}
